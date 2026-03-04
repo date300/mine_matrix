112b736fc288bf5f7cac1bdaa8fafcecabf4b8de
date300 <date300us@gmail.com>
2026-03-04 13:27:44 +0000

    Final Mine Matrix setup: fixed build configs, launcher icons, splash & workflow

--- lib/layout/topbar.dart
+++ lib/layout/topbar.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:glassmorphism/glassmorphism.dart';
-import 'package:reown_appkit/reown_appkit.dart'; // নতুন ইম্পোর্ট
+import 'package:reown_appkit/reown_appkit.dart';
 
 class TopBar extends StatefulWidget {
   const TopBar({super.key});
@@ -13,7 +13,7 @@ class TopBar extends StatefulWidget {
 }
 
 class _TopBarState extends State<TopBar> {
-  late ReownAppKitModal _appKitModal; // নতুন ক্লাস
+  ReownAppKitModal? _appKitModal;
   bool _isInitialized = false;
 
   final Color accentGreen = const Color(0xFF14F195);
@@ -22,10 +22,10 @@ class _TopBarState extends State<TopBar> {
   @override
   void initState() {
     super.initState();
-    _initializeAppKit();
+    _initializeReown();
   }
 
-  void _initializeAppKit() async {
+  void _initializeReown() async {
     _appKitModal = ReownAppKitModal(
       context: context,
       projectId: 'de4fd9cc5d44e0e8a830b232a38184da',
@@ -41,10 +41,9 @@ class _TopBarState extends State<TopBar> {
       ),
     );
 
-    await _appKitModal.init();
+    await _appKitModal!.init();
     
-    // কানেকশন স্ট্যাটাস ট্র্যাক করার জন্য লিসেনার
-    _appKitModal.addListener(_onServiceUpdate);
+    _appKitModal!.addListener(_onUpdate);
 
     if (mounted) {
       setState(() {
@@ -53,24 +52,22 @@ class _TopBarState extends State<TopBar> {
     }
   }
 
-  void _onServiceUpdate() {
-    if (mounted) setState(() {});
-  }
+  void _onUpdate() => setState(() {});
 
   @override
   void dispose() {
-    _appKitModal.removeListener(_onServiceUpdate);
+    _appKitModal?.removeListener(_onUpdate);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!_isInitialized) {
+    if (!_isInitialized || _appKitModal == null) {
       return SizedBox(height: 60.h);
     }
 
-    bool isConnected = _appKitModal.isConnected;
-    String? address = _appKitModal.session?.address;
+    bool isConnected = _appKitModal!.isConnected;
+    String? address = _appKitModal!.session?.address;
 
     String displayAddress = (isConnected && address != null)
         ? '${address.substring(0, 4)}...${address.substring(address.length - 4)}'
@@ -81,12 +78,12 @@ class _TopBarState extends State<TopBar> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          _buildBrandLogo(),
+          _buildLogo(),
           Row(
             children: [
-              _buildWalletButton(isConnected, displayAddress),
+              _buildWalletBtn(isConnected, displayAddress),
               SizedBox(width: 12.w),
-              _buildNotificationButton(),
+              _buildIconBtn(CupertinoIcons.bell_fill),
             ],
           ),
         ],
@@ -94,36 +91,35 @@ class _TopBarState extends State<TopBar> {
     );
   }
 
-  Widget _buildBrandLogo() {
+  Widget _buildLogo() {
     return Column(
-      mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("WEB3", style: GoogleFonts.inter(color: Colors.white60, fontSize: 10.sp, letterSpacing: 1.5)),
+        Text("WEB3", style: GoogleFonts.inter(color: Colors.white60, fontSize: 10.sp)),
         Text("MINE MATRIX", style: GoogleFonts.inter(color: Colors.white, fontSize: 22.sp, fontWeight: FontWeight.w900)),
       ],
     );
   }
 
-  Widget _buildWalletButton(bool isConnected, String displayAddress) {
+  Widget _buildWalletBtn(bool connected, String addr) {
     return GestureDetector(
-      onTap: () => _appKitModal.openModalView(), // নতুন মেথড
+      onTap: () => _appKitModal!.openModalView(),
       child: GlassmorphicContainer(
-        width: isConnected ? 135.w : 50.w,
+        width: connected ? 140.w : 50.w,
         height: 45.w,
         borderRadius: 15.r,
         blur: 15,
         alignment: Alignment.center,
         border: 1,
         linearGradient: LinearGradient(colors: [Colors.white.withOpacity(0.1), Colors.white.withOpacity(0.05)]),
-        borderGradient: LinearGradient(colors: [isConnected ? accentGreen.withOpacity(0.5) : accentPurple.withOpacity(0.5), Colors.transparent]),
+        borderGradient: LinearGradient(colors: [connected ? accentGreen : accentPurple, Colors.transparent]),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(isConnected ? CupertinoIcons.checkmark_seal_fill : CupertinoIcons.link, color: isConnected ? accentGreen : Colors.white, size: 20.sp),
-            if (isConnected) ...[
+            Icon(connected ? CupertinoIcons.checkmark_seal_fill : CupertinoIcons.link, color: connected ? accentGreen : Colors.white, size: 20.sp),
+            if (connected) ...[
               SizedBox(width: 8.w),
-              Text(displayAddress, style: GoogleFonts.inter(color: Colors.white, fontSize: 11.sp, fontWeight: FontWeight.bold)),
+              Text(addr, style: GoogleFonts.inter(color: Colors.white, fontSize: 11.sp, fontWeight: FontWeight.bold)),
             ]
           ],
         ),
@@ -131,7 +127,7 @@ class _TopBarState extends State<TopBar> {
     );
   }
 
-  Widget _buildNotificationButton() {
+  Widget _buildIconBtn(IconData icon) {
     return GlassmorphicContainer(
       width: 45.w,
       height: 45.w,
@@ -141,7 +137,7 @@ class _TopBarState extends State<TopBar> {
       border: 1,
       linearGradient: LinearGradient(colors: [Colors.white.withOpacity(0.1), Colors.white.withOpacity(0.05)]),
       borderGradient: LinearGradient(colors: [Colors.white.withOpacity(0.2), Colors.transparent]),
-      child: Icon(CupertinoIcons.bell_fill, color: accentGreen, size: 22.sp),
+      child: Icon(icon, color: accentGreen, size: 22.sp),
     );
   }
 }
